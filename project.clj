5e08f78665a65cf9bf2287358bc5056bd82f943d
Gou <nakamura462645@gmail.com>
2020-02-26 15:53:19 +0900

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject texdata "0.1.0-SNAPSHOT"
+(defproject texdata "0.1.1-SNAPSHOT"
   :description "Converts Clojure data to TeX strings."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
