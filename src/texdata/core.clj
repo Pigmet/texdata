3adae5315aea43884bc73114894d1ebdc07c27cf
Gou <nakamura462645@gmail.com>
2020-03-01 11:56:59 +0900

    added register-command

--- src/texdata/core.clj
+++ src/texdata/core.clj
@@ -20,18 +20,47 @@
     (swap! the-atom assoc k v))
   @the-atom)
 
-(defn- register-command [& kvs]
+(defn- register-command-impl [& kvs]
   (register-impl command-repository kvs))
+
+(defn register-command
+  ([id type] (register-command id type (name id)))
+  ([id type s] (register-command-impl id {:type type :s s})))
 
 (def ^:private decorator-reposity (atom {}))
 
 (defn- register-decorator [& kvs]
   (register-impl decorator-reposity kvs))
 
+(defn- get-decorator [id] (get @decorator-reposity id))
+
+(s/def ::decorate-spec
+  (s/+ (s/cat :k get-decorator :v any?)))
+
+(s/def ::command-spec (s/cat :cmd keyword? :opt map? :args (s/* any?)))
+
 (s/def ::tex-spec
   (s/or
-   :literal (some-fn string? number?)))
+   :literal (some-fn string? number?)
+   :independent keyword?
+   :decorated (s/cat :v any? :decorate ::decorate-spec)
+   :command ::command-spec
+   :chunk coll?))
+
+(defn- tex-data-type [x]
+  (key(s/conform ::tex-spec x)))
+
+(defmulti data->string {:private true} tex-data-type)
+
+(defn tex[& args] (join " " (map data->string args)))
+
+(defmethod data->string :literal[x] (str x))
+
+(defmethod data->string :chunk[x] (apply tex x))
 
 
-hoge 
+
+
+
+
 
