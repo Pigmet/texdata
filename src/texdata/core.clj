080e06eaaec14c35ac865fffa45b5eaaf9087de3
Gou <nakamura462645@gmail.com>
2020-03-03 12:32:36 +0900

    added tex->

--- src/texdata/core.clj
+++ src/texdata/core.clj
@@ -57,129 +57,154 @@
 
 (defmulti data->string {:private true} tex-data-type)
 
+;; tex does little more than calling data->string, which is
+;; a multi-method for converting various data to TeX string.
+;; Its dispatch is done based on the type of data
+;; specified by the spec ::tex-spec.
+
 (defn tex [& args] (join " " (map data->string args)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; tex threading  ;;
+;;;;;;;;;;;;;;;;;;;;
+
+(defn tex->> [exp & more]
+  (reduce
+   (fn [acc x]
+     (if (coll? x) (tex (concat x [acc])) (tex [x acc])))
+   (tex exp)
+   more))
+
+(defn tex-> [exp & more]
+  (reduce
+   (fn [acc x]
+     (if (coll? x) (tex (concat [acc] x)) (tex [x acc])))
+   (tex exp)
+   more))
+
+;; data->string 
 
 (defmethod data->string :literal[x] (str x))
 
 (defmethod data->string :chunk[x] (apply tex x))
 
 (defn- tex-sub [data v]
-  (format "%s_{%s}" (tex data) (tex v)))
+(format "%s_{%s}" (tex data) (tex v)))
 
 (defn tex-pow [data v]
-  (format "%s^{%s}" (tex data) (tex v)))
+(format "%s^{%s}" (tex data) (tex v)))
 
 (defn- decorate-tex-impl [data decorators m]
-  (reduce-kv
-   (fn [acc k v]
-     (let [f (get decorators k)]
-       (f acc v)))
-   (tex data)
-   m))
+(reduce-kv
+ (fn [acc k v]
+   (let [f (get decorators k)]
+     (f acc v)))
+ (tex data)
+ m))
 
 (defn- decorate-tex [data m]
-  (decorate-tex-impl data @decorator-reposity m))
+(decorate-tex-impl data @decorator-reposity m))
 
 (register-decorator :pow tex-pow :sub tex-sub)
 
 (defmethod data->string :decorated [[v & more]]
-  (let [m (apply hash-map more)]
-    (decorate-tex-impl v @decorator-reposity m)))
+(let [m (apply hash-map more)]
+  (decorate-tex-impl v @decorator-reposity m)))
 
 (defmethod data->string :independent [x]
-  (if-let [s (-> x get-command :s)] (str "\\" s ) (str "\\" (name x))))
+(if-let [s (-> x get-command :s)] (str "\\" s ) (str "\\" (name x))))
 
 (defn- conform-command [data]
-  (s/conform ::command-spec data))
+(s/conform ::command-spec data))
 
 (defmulti env-command {:private true} first)
 
 (defmulti normal-command {:private true} first)
 
 (defmethod data->string :command [data]
-  (case (-> data first get-command :type)
-    :normal (normal-command data)
-    :environment (env-command data)))
+(case (-> data first get-command :type)
+  :normal (normal-command data)
+  :environment (env-command data)))
 
 (defmethod env-command :default [data]
-  (let[{:keys [cmd opt args]} (conform-command data)
-       cmd (if-let [s (-> cmd get-command :s)] s (name cmd))]
-    (-> (format "\\begin{%s}" cmd)
-        (decorate-tex-impl @decorator-reposity opt)
-        (str (format " %s \\end{%s}" (tex args) cmd)))))
+(let[{:keys [cmd opt args]} (conform-command data)
+     cmd (if-let [s (-> cmd get-command :s)] s (name cmd))]
+  (-> (format "\\begin{%s}" cmd)
+      (decorate-tex-impl @decorator-reposity opt)
+      (str (format " %s \\end{%s}" (tex args) cmd)))))
 
 (defmethod normal-command :default [data]
-  (let[{:keys [cmd opt args]} (conform-command data)
-       cmd (if-let [s (-> cmd get-command :s)] s (name cmd))]
-    (-> (format "\\%s" cmd)
-        (decorate-tex-impl @decorator-reposity opt)
-        (str (format "{%s}" (tex args)))))) 
+(let[{:keys [cmd opt args]} (conform-command data)
+     cmd (if-let [s (-> cmd get-command :s)] s (name cmd))]
+  (-> (format "\\%s" cmd)
+      (decorate-tex-impl @decorator-reposity opt)
+      (str (format "{%s}" (tex args)))))) 
 
 (s/def ::defcmd-spec
-  (s/cat :id keyword?
-         :type #{:environment :normal :independent}
-         :body (s/+ any?)))
+(s/cat :id keyword?
+       :type #{:environment :normal :independent}
+       :body (s/+ any?)))
 
 (defmacro defcmd
-  "Defines a new tex command."
-  [id type & body]
-  {:pre [(s/valid? ::defcmd-spec (list* id type body))]}
-  (let [{:keys [id type body]} (s/conform ::defcmd-spec (list* id type body))
-        fn-table {:environment `env-command
-                  :normal `normal-command}
-        default? (-> body first (= :default))]
-    (cond
-      default? `(register-command ~id ~type)
-      (= type :independent) `(register-command ~id ~type ~(first body))
-      :else `(do
-               (register-command ~id ~type)
-               (defmethod ~(get fn-table type) ~id ~@body)))))
+"Defines a new tex command."
+[id type & body]
+{:pre [(s/valid? ::defcmd-spec (list* id type body))]}
+(let [{:keys [id type body]} (s/conform ::defcmd-spec (list* id type body))
+      fn-table {:environment `env-command
+                :normal `normal-command}
+      default? (-> body first (= :default))]
+  (cond
+    default? `(register-command ~id ~type)
+    (= type :independent) `(register-command ~id ~type ~(first body))
+    :else `(do
+             (register-command ~id ~type)
+             (defmethod ~(get fn-table type) ~id ~@body)))))
 
 (def ^:private  example-repository (atom{}))
 
 (defn register-example [& kvs]
-  (doseq [[k v] (partition 2 kvs)]
-    (swap! example-repository assoc k v)
-    @example-repository))
+(doseq [[k v] (partition 2 kvs)]
+  (swap! example-repository assoc k v)
+  @example-repository))
 
 (defn example [id] (get @example-repository id))
 
 ;; commands
 
 (defmacro pre-check-tex [test id data]
-  (let[m {:input data}
-       ex (example id)
-       m (if ex (assoc m :expected-input ex) m)]
-    `(or ~test
-         (throw
-          (ex-info ~(format "invalid input for %s" id)
-                   ~m)))))
+(let[m {:input data}
+     ex (example id)
+     m (if ex (assoc m :expected-input ex) m)]
+  `(or ~test
+       (throw
+        (ex-info ~(format "invalid input for %s" id)
+                 ~m)))))
 
 ;; basic math 
 
 (defcmd :int :normal [data]
-  (let [{:keys [opt args]} (conform-command data)]
-    (-> "\\int"
-        (decorate-tex opt)
-        (str (format " %s" (tex args))))))
+(let [{:keys [opt args]} (conform-command data)]
+  (-> "\\int"
+      (decorate-tex opt)
+      (str (format " %s" (tex args))))))
 
 (register-decorator
- :from tex-sub
- :on tex-sub
- :to tex-pow)
+:from tex-sub
+:on tex-sub
+:to tex-pow)
 
 (defcmd :math :normal [[_ & args]]
-  (format "\\[ %s \\]" (tex args)))
+(format "\\[ %s \\]" (tex args)))
 
 (defcmd :frac :normal [[_ x y]]
-  (format "\\frac{%s}{%s}" (tex x) (tex y)))
+(format "\\frac{%s}{%s}" (tex x) (tex y)))
 
 ;; preamble
 
 (s/def ::documentclass-spec
-  (s/cat :cmd keyword?
-         :opt (s/? (s/map-of #{:opt} (s/coll-of any?)))
-         :args any?))
+(s/cat :cmd keyword?
+       :opt (s/? (s/map-of #{:opt} (s/coll-of any?)))
+       :args any?))
 
 (register-example
  :documentclass
@@ -215,6 +240,9 @@
 (defcmd :huge :environment :default)
 
 (defcmd :Huge :environment :default)
+
+(defcmd :color :normal [[_ c & more]]
+  (format "\\color{%s}{%s}" (tex c) (tex more)))
 
 
 
