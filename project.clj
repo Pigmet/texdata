ed2beb5ccf1b9dd0d2154bff09839b595c1edbf4
Gou <nakamura462645@gmail.com>
2020-05-09 20:51:01 +0900

    added defmethod normal-command :documentclass

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [expound "0.8.4"]
                  [org.apache.pdfbox/pdfbox "2.0.19"]]
   :repl-options {:init-ns texdata.core}
   :deploy-repositories [["releases" :clojars]
--- src/texdata/core.clj
+++ src/texdata/core.clj
@@ -24,3 +24,77 @@
    :none ["." "."]})
 
 ;; specs
+
+(def command-types #{:environment :normal :single})
+
+(def commands (atom {}))
+
+(defn- register-cmd!
+  ([type k] (register-cmd! type k nil))
+  ([type k ex]
+   (swap! commands assoc k {:type type :example ex})))
+
+(defn example
+  "Returns example data for this command."
+  [k]
+  (get-in @commands [k :example ] ))
+
+(s/def ::tex-spec
+  (s/or :literal (some-fn string? number?)
+        :single keyword?
+        :normal #(->> % first (get @commands) :type (= :normal) )
+        :environment #(->> % first (get @commands) :type (= :environment) )))
+
+(defn- tex-data-type
+  "Returns one of the tex-spec keys if x conforms to it, or nil."
+  [x]
+  (when (s/valid? ::tex-spec x)
+    (key (s/conform ::tex-spec x))))
+
+;; example
+
+;; main fn
+
+(defmulti data->string tex-data-type)
+
+(defmethod data->string :literal [x] (str x))
+
+(defmethod data->string :single [x] (str "\\" (name x)))
+
+(defmulti environment-command first)
+
+(defmulti normal-command first)
+
+(defmethod data->string :normal [data] (normal-command data))
+
+(defmethod data->string :environment [data] (environment-command data))
+
+;; impl
+
+(s/def ::documentclass-spec
+  (s/&  (s/cat :cmd keyword?
+               :class string?
+               :opt (s/? (s/cat :k #{:opt} :v (s/coll-of string?))))
+        (s/conformer (fn [{cl :class {v :v} :opt}]
+                       {:class cl :opt v}))))
+
+(s/conform ::documentclass-spec
+           [:documentclass "a" :opt [1 2 3] 9])
+
+(defmethod normal-command :documentclass
+  [data]
+  (let [{cl :class opt :opt} (s/conform ::documentclass-spec data)]
+    (format "\\documentclass[%s]{%s}" (join "," opt) cl  )))
+
+(register-cmd! :normal
+               :documentclass
+               [[:documentclass "article"]
+                [:documentclass "article" :opt ["letterpaper" "12pt"]]])
+
+(data->string [:documentclass "article" :opt ["letterpaper" "12pt"]])
+
+(example :documentclass)
+
+
+
+
