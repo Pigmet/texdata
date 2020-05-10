96c81ba40c25e668df52bb6408a62ef9c84caad0
Gou <nakamura462645@gmail.com>
2020-05-10 19:28:54 +0900

    added defn- get-siblings [f]

--- src/texdata/compile.clj
+++ src/texdata/compile.clj
@@ -1,4 +1,28 @@
 (ns texdata.compile
   (:require [clojure.java.io :refer [file]]
-            [clojure.java.shell :refer [sh with-sh-dir]]))
+            [clojure.java.shell :refer [sh with-sh-dir]])
+  (:import (java.nio.file Files Paths)))
+
+(def temp-file (file "resources/temp.tex"))
+
+(defn- file-data [f]
+  {:absolute-path (.getAbsolutePath f)
+   :canonical-path (.getCanonicalPath f)
+   :parent (.getParent f)
+   :directory? (.isDirectory f)
+   :name (.getName f)})
+
+(defn- get-siblings [f]
+  (let [{:keys [directory? parent]} (file-data f)]
+    (if directory?
+      (.listFiles f)
+      (-> parent file (.listFiles) ))))
+
+(comment
+  (->> temp-file
+       get-siblings
+       (map file-data)
+       (map :name)))
+
+
 
